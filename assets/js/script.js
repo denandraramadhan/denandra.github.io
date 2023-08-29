214e9bc499994f99a8d113f148fda350bcd65325
denikurnia2000 <denikurnia111000@gmail.com>
2023-08-30 04:59:16 +0800

    Add Hero Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
+// Navbar
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
     header.classList.toggle("sticky", window.scrollY > 120);
+});
+
+// Typed text
+const typed = new Typed("#typing", {
+    strings: ["Deni Kurnia", "a Fullstack Web Developer", "a Fresh Graduate"],
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 100,
+    backDelay: 1000,
 });
