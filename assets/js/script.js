0ee2a58deb818686eca6e1781a056b64077a1671
denikurnia2000 <denikurnia111000@gmail.com>
2023-08-30 16:59:12 +0800

    Make Responsive Hero Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,17 @@ window.addEventListener("scroll", () => {
     header.classList.toggle("sticky", window.scrollY > 120);
 });
 
+// Hamburger Menu / Sidebar
+const hamburger = document.querySelector("#menu-icon");
+const navList = document.querySelector("#navList");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("bx-x");
+    navList.classList.toggle("active");
+});
+
 // Typed text
 const typed = new Typed("#typing", {
-    strings: ["Deni Kurnia", "a Fullstack Web Developer", "a Fresh Graduate"],
+    strings: ["Deni Kurnia", "a Fullstack Developer", "a Fresh Graduate"],
     typeSpeed: 100,
     loop: true,
     backSpeed: 100,
