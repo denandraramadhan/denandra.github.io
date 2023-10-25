8a79eb1105a2b795d3f7974ffe4098d7cf645994
denikurnia2000 <denikurnia111000@gmail.com>
2023-10-26 02:57:02 +0800

    Add Skils and Project

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,17 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-    header.classList.toggle("sticky", window.scrollY > 120);
+    header.classList.toggle("shadow-lg", window.scrollY > 120);
+    header.classList.toggle("!bg-[#22282f]", window.scrollY > 120);
 });
 
 // Hamburger Menu / Sidebar
-const hamburger = document.querySelector("#menu-icon");
-const navList = document.querySelector("#navList");
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("bx-x");
-    navList.classList.toggle("active");
-});
+// const hamburger = document.querySelector("#menu-icon");
+// const navList = document.querySelector("#navList");
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("bx-x");
+//     navList.classList.toggle("active");
+// });
 
 // Typed text
 const typed = new Typed("#typing", {
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                main: "#0ef",
+                bgMain: "#1b1f24",
+                other: "#c3cad5",
+            },
+        },
+    },
+    plugins: [],
+};
