e2ece6b8a99a091e0336730623ccf197c793acd0
denikurnia2000 <denikurnia111000@gmail.com>
2023-10-29 03:15:05 +0800

    Add some section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
                 bgMain: "#1b1f24",
                 other: "#c3cad5",
             },
+            keyframes: {
+                animate: {
+                    "0%": { transform: "translateX(100%)" },
+                    "100%": { transform: "translateX(-100%)" },
+                },
+                animate2: {
+                    "0%": { transform: "translateX(0)" },
+                    "100%": { transform: "translateX(-200%)" },
+                },
+            },
         },
     },
     plugins: [],
